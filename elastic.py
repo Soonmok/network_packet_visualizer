fb51abde2e99c5ed7dc2682ab92ac2040589fb49
Soonmok <tnsahr2580@gmail.com>
2019-06-04 00:15:10 +0900

    filtered packet logs

--- elastic.py
+++ elastic.py
@@ -4,9 +4,12 @@ from elasticsearch_dsl import Search
 client = Elasticsearch()
 
 s = Search(using=client, index="logstash*") \
-        .query("match", src_ip="172.217.25.106")
+        .exclude("multi_match", query="_grokparsefailure 8.8.8.8", fields=['tags', 'src_ip']) \
+        .filter("range", **{"len":{"from":0}})
 
+total = s.count()
+print("all hits : {}".format(total))
 response = s.execute()
 
-for hit in response:
-    print(hit.src_ip)
+for hit in s.scan():
+    print(hit.len)
