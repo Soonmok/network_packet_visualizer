741e378dbb39f746e3195c17e151d298894ef3ba
Soonmok <tnsahr2580@gmail.com>
2019-06-04 21:54:31 +0900

    devided by source_ip

--- elastic.py
+++ elastic.py
@@ -12,13 +12,21 @@ total = s.count()
 print("all hits : {}".format(total))
 response = s.execute()
 
-idxes = []
-hits = []
+normal_idxes = []
+normal_hits = []
+dos_idxes = []
+dos_hits = []
 
 for idx, hit in enumerate(s.scan()):
-    idxes.append(idx)
-    hits.append(hit.len)
+    if hit.src_ip == "111.111.111.222":
+        normal_idxes.append(idx)
+        normal_hits.append(hit.len)
+    else:
+        dos_idxes.append(idx)
+        dos_hits.append(hit.len)
 
-plt.scatter(idxes, hits)
+
+plt.scatter(dos_idxes, dos_hits, c='tab:blue')
+plt.scatter(normal_idxes, normal_hits, c='tab:orange')
 plt.yscale('log')
 plt.show()
