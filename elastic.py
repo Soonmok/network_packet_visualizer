9013d96eddcd68c1e1aa943b8c4b1b6ccd4eb896
Soonmok <tnsahr2580@gmail.com>
2019-06-07 21:17:54 +0900

    added 6 windows

--- elastic.py
+++ elastic.py
@@ -10,62 +10,101 @@ client = Elasticsearch()
 
 # search hits
 s = Search(using=client, index="logstash*") \
-        .exclude("multi_match", query="_grokparsefailure 8.8.8.8", fields=['tags', 'src_ip']) \
-        .filter("range", **{"len":{"from":0}}) \
-        .filter("range", **{"ttl":{"from":0}})
+        .filter("exists", field="src_ip") \
+        .filter("exists", field="ttl") \
+        .filter("exists", field="len") \
+        .filter("range", **{"@timestamp":{'gte': 'now-29m', 'lt': 'now'}})
+        #.exclude("multi_match", query="8.8.8.8 192.168.56.12", fields=['src_ip', 'dst_ip']) \
 
 # configure pyplot windows
 fig = plt.figure()
-ax1 = fig.add_subplot(2, 1, 1)
-ax2 = fig.add_subplot(2, 1, 2)
+ax1 = fig.add_subplot(2, 3, 1)
+ax2 = fig.add_subplot(2, 3, 2)
+ax3 = fig.add_subplot(2, 3, 3)
+ax4 = fig.add_subplot(2, 3, 4)
+ax5 = fig.add_subplot(2, 3, 5)
+ax6 = fig.add_subplot(2, 3, 6)
+
+def init_dict():
+    # making dictionray
+    src_ips = {}
+    src_ips['192.168.56.12'] = {}
+    src_ips['192.168.56.12']['cnt'] = 0
+    src_ips['192.168.56.12']['len'] = []
+    src_ips['192.168.56.12']['ttl'] = []
+    src_ips['192.168.56.12']['dport'] = []
+
+    src_ips['another_ip'] = {}
+    src_ips['another_ip']['cnt'] = 0
+    src_ips['another_ip']['ttl'] = []
+    src_ips['another_ip']['len'] = []
+    src_ips['another_ip']['dport'] = []   
+    return src_ips
 
 # process data
 def get_data(s):
     total = s.count()
     print("all hits : {}".format(total))
     s.execute()
+    src_ips = init_dict()
 
-    # making dictionray
-    src_ips = {}
     for idx, hit in enumerate(s.scan()):
         # counting packets
-        if hit.src_ip in src_ips:
+        if hit.src_ip == '192.168.56.12':
             src_ips[hit.src_ip]['cnt'] += 1
             src_ips[hit.src_ip]['ttl'].append(hit.ttl)
             src_ips[hit.src_ip]['len'].append(hit.len)
+            src_ips[hit.src_ip]['dport'].append(hit.dpt)
         else:
-            src_ips[hit.src_ip] = {}
-            src_ips[hit.src_ip]['cnt'] = 1
-            src_ips[hit.src_ip]['ttl'] = []
-            src_ips[hit.src_ip]['len'] = []
+            src_ips['another_ip']['cnt'] += 1
+            src_ips['another_ip']['ttl'].append(hit.ttl)
+            src_ips['another_ip']['len'].append(hit.len)
+            src_ips['another_ip']['dport'].append(hit.dpt)
+    print(src_ips.keys())
+    return src_ips
     
-    keys = {}
-    cnts = []
-    for key in list(src_ips.keys()):
-        if src_ips[key]['cnt'] > 100:
-            keys[key] = {}
-            cnts.append(src_ips[key]['cnt'])
-            keys[key]['ttl'] = src_ips[key]['ttl']
-            keys[key]['len'] = src_ips[key]['len']
-    return keys, cnts
+def draw_line(keys, ax):
+    ax.scatter(c='tab:orange', label="normal")
+    ax.scatter(c='tab:green', label="dos")
+
+def draw_bar(keys, ax, cnts):
+    ax.bar(list(keys.keys()), cnts, align='center', alpha=0.5)
+       
+def draw_scatter(keys, ax, x_axis, y_axis):
+    ax.scatter(
+            keys['192.168.56.12'][x_axis], 
+            keys['192.168.56.12'][y_axis], 
+            c='tab:green', label="dos")
+    ax.scatter(
+            keys['another_ip'][x_axis], 
+            keys['another_ip'][y_axis], 
+            c='tab:orange', label="normal")
+ 
+# def filter_keys(src_ips):
+#     keys = {}
+#     cnts = []
+#     for key in list(src_ips.keys()):
+#         if src_ips[key]['cnt'] > 1:
+#             keys[key] = {}
+#             cnts.append(src_ips[key]['cnt'])
+#             keys[key]['ttl'] = src_ips[key]['ttl']
+#             keys[key]['len'] = src_ips[key]['len']
+#             keys[key]['dport'] = src_ips[key]['dport']
+#     return keys, cnts
+   
 
 # update pyplot window
-def update(frame):
-    keys, cnts = get_data(s)
-    ttl_indexes = np.argsort(keys['192.168.56.12']['ttl'])
-    ttls = np.array(keys['192.168.56.12']['ttl'])
-    ax1.scatter(
-            ttls[ttl_indexes], 
-            keys['192.168.56.12']['len'], 
-            c='tab:green', label="dos")
-    ax1.scatter(
-            keys['111.111.111.222']['ttl'], 
-            keys['111.111.111.222']['len'], 
-            c='tab:orange', label="normal")
-    keys['192.168.56.12']['ttl'] = np.array(
-            keys['192.168.56.12']['ttl'])
-    ax2.bar(list(keys.keys()), cnts, align='center', alpha=0.5)
-    return ax1, ax2 
+def update(s):
+    keys = get_data(s)
+    #keys, cnts = filter_keys(src_ips)
+    #draw_line(keys, ax1)
+    #draw_bar(keys, ax2, cnts)
+    #draw_bar(keys, ax3, cnts)
+    print(keys)
+    draw_scatter(keys, ax4, x_axis='dport', y_axis='ttl')
+    draw_scatter(keys, ax5, x_axis='len', y_axis='dport')
+    draw_scatter(keys, ax6, x_axis='len', y_axis='ttl')
 
-animation = FuncAnimation(fig, update, interval=200)
+#animation = FuncAnimation(fig, lambda x: update(s), interval=100)
+update(s)
 plt.show()
